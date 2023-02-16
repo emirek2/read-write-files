45bdfd0bfb47c15106ccdaf55cbf7f37d713adeb
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2023-02-16 18:44:56 +0000

    things

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 # remember to fork this and push to github
 
+
+
+# f=open('test.txt','a')
+# f.write(' ur mom')
+# # read gives us every thing
+# # readline gives the first line
+# # print(f.readline())
+
+
+# f.close()
+# f=open('employees.txt', 'a')
+# f.write("\nPedro - bitch")
+# f.close()
+
+# with open('test.txt','r') as f:
+#   print(f.read())
+#   f.write('bdlhvbajhabhvjalbjzhbvlabvb')
+#   print(f)
+#   for line in f:
+#     print(line,end='')
+
+with open('period7.txt','w') as f:
+  f.write('this mess is over')
